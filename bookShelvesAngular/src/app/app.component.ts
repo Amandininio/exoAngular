797b821208d338ad6be987acc1eb0867acbd3cd5
Amandininio <amandiomota@gmail.com>
2019-08-16 09:44:15 +0200

    almost here

---
+++ bookShelvesAngular/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAX7gBRZ2OeV7KTg_MZsHhq2-E0iJcsoYI',
+      authDomain: 'bookshelvesangular.firebaseapp.com',
+      databaseURL: 'https://bookshelvesangular.firebaseio.com',
+      projectId: 'bookshelvesangular',
+      storageBucket: '',
+      messagingSenderId: '262713355978',
+      appId: '1:262713355978:web:63221a05191bca8c'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+}
+
---
+++ bookShelvesAngular/src/app/book-list/book-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from '../models/books.model';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit, OnDestroy {
+  books: Book[];
+  bookSubscription: Subscription;
+
+  constructor(private booksService: BooksService, private router: Router) { }
+
+  ngOnInit() {
+    this.bookSubscription = this.booksService.bookSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  ngOnDestroy() {
+    this.bookSubscription.unsubscribe();
+  }
+
+}
